5bab6652120d1096448acd2befad29ef225ccb18
Filtee <filtee0812@gmail.com>
2023-11-22 17:00:05 +0800

    Lab02 updated!

--- Lab02/src/main/HuffmanTree.java
+++ Lab02/src/main/HuffmanTree.java
@@ -1,16 +1,19 @@
 package main;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class HuffmanTree {
-    private Map<String, String> codeTable = new HashMap<>();
-    private Map<String, String> decodeTable = new HashMap<>();
+    /****************************************************************
+     *                private methods and properties                *
+     ****************************************************************/
+
+    private Map<Character, Integer> frequencyTable;
+    private Map<String, String> codeTable;
+    private Map<String, String> decodeTable;
 
     private void huffmanTree() {
         MinPQ<String> minPQ = new MinPQ<>();
     }
-
 
     private void huffmanBuild() {
 
@@ -18,5 +21,44 @@ public class HuffmanTree {
 
     private void huffmanCode() {
 
+    }
+
+    /****************************************************************
+     *                 public methods and properties                *
+     ****************************************************************/
+
+    public HuffmanTree() {
+        this.codeTable = new HashMap<>();
+        this.decodeTable = new HashMap<>();
+    }
+
+    /**
+     * Stores the frequency table into the Huffman tree.
+     * Key is the character, value is the frequency.
+     *
+     * @param frequencyTable the frequency table
+     */
+    public void Store(HashMap<Character, Integer> frequencyTable) {
+        this.frequencyTable = frequencyTable;
+    }
+
+    /**
+     * Returns the frequency table.
+     * Key is the character, value is the frequency.
+     *
+     * @return the frequency table
+     */
+    public Map<String, String> getCodeTable() {
+        return codeTable;
+    }
+
+    /**
+     * Returns the decode table.
+     * Key is the code, value is the character.
+     *
+     * @return the decode table
+     */
+    public Map<String, String> getDecodeTable() {
+        return decodeTable;
     }
 }
