e7eac06beb0ad987d92b349a31ec00281bdeab01
Filtee <filtee0812@gmail.com>
2023-11-22 16:23:14 +0800

    Lab02 update

---
+++ Lab02/src/main/HuffmanTree.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HuffmanTree {
+    private Map<String, String> codeTable = new HashMap<>();
+    private Map<String, String> decodeTable = new HashMap<>();
+
+    private void huffmanTree() {
+        MinPQ<String> minPQ = new MinPQ<>();
+    }
+
+
+    private void huffmanBuild() {
+
+    }
+
+    private void huffmanCode() {
+
+    }
+}
